7f9875fb21d2b1bb066eb40707a2cadc951abd0b
nikhil3992 <nikhil3992@gmail.com>
2018-03-18 23:07:30 -0700

    Added Some Test Class

---
+++ app/src/main/java/com/nikhil/testapp/SomeTestClass.java
@@ -0,0 +1,27 @@
+package com.nikhil.testapp;
+
+/**
+ * Created by Surya Prakasam on 18-03-2018.
+ */
+
+public class SomeTestClass {
+
+    private String someString;
+    private int someInt;
+
+    public String getSomeString() {
+        return someString;
+    }
+
+    public void setSomeString(String someString) {
+        this.someString = someString;
+    }
+
+    public int getSomeInt() {
+        return someInt;
+    }
+
+    public void setSomeInt(int someInt) {
+        this.someInt = someInt;
+    }
+}
