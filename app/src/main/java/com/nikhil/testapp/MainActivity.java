fcd3948d0a113ba266d5f1c772d06630eda99b38
nikhil3992 <nikhil3992@gmail.com>
2018-03-18 18:56:07 -0700

    Added for loop

--- app/src/main/java/com/nikhil/testapp/MainActivity.java
+++ app/src/main/java/com/nikhil/testapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.nikhil.testapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        for(int i=0;i<10;i++) {
+            Log.d("nikhil","i is "+i);
+        }
+
     }
 }
